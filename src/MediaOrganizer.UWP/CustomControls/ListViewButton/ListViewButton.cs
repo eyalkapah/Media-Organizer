1b13e865aacaba6e210a74ea8a0a99007404a531
Eyal.Kapah <eykapah@microsoft.com>
2019-12-30 16:15:20 +0200

    work on the listviewbutton control

--- src/MediaOrganizer.UWP/CustomControls/ListViewButton/ListViewButton.cs
+++ src/MediaOrganizer.UWP/CustomControls/ListViewButton/ListViewButton.cs
@@ -15,6 +15,16 @@ namespace MediaOrganizer.UWP.CustomControls.ListViewButton
 {
     public sealed class ListViewButton : Control
     {
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(ListViewButton), new PropertyMetadata(default));
+
         public ListViewButton()
         {
             DefaultStyleKey = typeof(ListViewButton);
